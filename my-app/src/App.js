504c9e87e578b48fe69fa2eba6476d73bb43a45a
Mahipal <mahipal.choudhary10@gmail.com>
2017-11-05 10:22:03 +0530

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import AgGridComponent from './AgGridComponent/AgGridComponent'
+/*This is comment*/
 
 class App extends Component {
     render() {
